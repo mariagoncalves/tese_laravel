189f8eb838d78adf851db23f6af2b33ad2194a9e
Maria <mariah.jog@gmail.com>
2017-10-14 14:24:42 +0100

    Continuação do pesquisa dinâmica - implementação de nova maneira de pesquisar conforme o que está selecionado

--- app/Http/Controllers/DynamicSearchController.php
+++ app/Http/Controllers/DynamicSearchController.php
@@ -8,6 +8,7 @@ use App\PropAllowedValue;
 use App\Property;
 use App\RelType;
 use App\Entity;
+use App\Value;
 use App\Relation;
 
 class DynamicSearchController extends Controller
@@ -249,7 +250,7 @@ class DynamicSearchController extends Controller
         $result      = [];
         $query       = [];
 
-        \Log::debug($data);
+        //\Log::debug($data);
         /*\Log::debug("Id da propriedade");
         \Log::debug($data['checkET']);
 
@@ -259,8 +260,7 @@ class DynamicSearchController extends Controller
 
         // Formar a query para apresentar os dados na tabela
         //Query base para a pesquisa
-        $query = Entity::where('ent_type_id', $idEntityType)
-                        ->with(['language' => function($query) use ($language_id) {
+        $query = Entity::with(['language' => function($query) use ($language_id) {
                             $query->where('language_id', $language_id);
                         }])
                         ->with(['values' => function($query) use ($language_id) {
@@ -276,6 +276,9 @@ class DynamicSearchController extends Controller
         //Formar a frase e realizar pesquisa de acordo com a pesquisa..
         $phrase = $this->formPhraseAndQuery($idEntityType, $data, $query);
 
+        \Log::debug("DADOS TESEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE (GERAL PESQUISA): ");
+        \Log::debug($query->toSql());
+
         $result['phrase'] = $phrase;
         $result['result'] = $query->get()->toArray();
         
@@ -285,61 +288,93 @@ class DynamicSearchController extends Controller
     public function formPhraseAndQuery($idEntityType, $data, &$query) {
         $language_id = 1;
 
-        $ent = EntType::with(['language' => function($query) use ($language_id) {
-                        $query->where('language_id', $language_id);
+        $ent = EntType::with(['language' => function($q) use ($language_id) {
+                        $q->where('language_id', $language_id);
                     }])->find($idEntityType);
 
         $phrase[] = "Pesquisa de todas as entidades do tipo ".$ent->language->first()->pivot->name;
 
+        \Log::debug("DADOS TESEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE (TABELA 1): ");
+
         // Formar a frase da tabela 1 e pesquisar de acordo com a pesquisa efetuada na tabela 1
+        $queryTable1 = new Entity;
         for ($i=0; $i < $data['numTableET']; $i++) { 
             if (isset($data['checkET'.$i])) {
-                $phrase = $this->formPhraseAndQueryTableType($data, $data['checkET'.$i], 'ET', $i, $phrase, $query);
+                $phrase = $this->formPhraseTableType($data, $data['checkET'.$i], 'ET', $i, $phrase, $query);
+
+                $this->formQueryTable1AndTable2($data, $data['checkET'.$i], 'ET', $i, $queryTable1);
             }
         }
 
+        //\Log::debug($queryTable1->distinct('id')->toSql());
+        $resultTable1 = $queryTable1->distinct('id')->get(['id'])->toArray();
+
+        $entitiesIdsTable1 = $this->formatArrayData($resultTable1, 'id');
+        \Log::debug($entitiesIdsTable1);
+
+        // Adicionar a query geral filtros da tabela 1 
+        $query = $query->where(function($q) use ($idEntityType, $entitiesIdsTable1) {
+                $q->where('ent_type_id', $idEntityType)->whereIn('id', $entitiesIdsTable1);
+            });
+
+        \Log::debug("DADOS TESEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE (TABELA 2): ");
+
         // Formar a frase da tabela 2
-        for ($i=0; $i < $data['numTableVT']; $i++) { 
-            if (isset($data['checkVT'.$i])) {                
-                $phrase = $this->formPhraseAndQueryTableType($data, $data['checkVT'.$i], 'VT', $i, $phrase, $query);
+        $selectTable2 = false;
+        $queryTable2  = new Entity;
+        for ($i = 0; $i < $data['numTableVT']; $i++) { 
+            if (isset($data['checkVT'.$i])) {
+                $selectTable2 = true;                
+                $phrase = $this->formPhraseTableType($data, $data['checkVT'.$i], 'VT', $i, $phrase, $query);
+
+                $this->formQueryTable1AndTable2($data, $data['checkVT'.$i], 'VT', $i, $queryTable2);
             }
         }
+
+        /*foreach ($entitiesIdsTable1 as $id_entity) {
+            $name = Entity::find($id_entity)->with('');
+
+            $query = $query->whereHas('values', function($q) use ($name) {
+                                $q->where('value', $name);
+                            });
+        }*/
+
+        //\Log::debug($queryTable2->distinct('id')->toSql());
+        $resultTable2 = $queryTable2->distinct('id')->get(['id'])->toArray();
+
+        $entitiesIdsTable2 = $this->formatArrayData($resultTable2, 'id');
+        \Log::debug($entitiesIdsTable2);
+
+        if ($selectTable2 == true) {
+            // Adicionar a query geral filtros da tabela 2 
+            $query = $query->OrWhere(function($q) use ($idEntityType, $entitiesIdsTable2) {
+                        $q->whereIn('id', $entitiesIdsTable2);
+                    });
+
+        }
+
+        \Log::debug("DADOS TESEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE (TABELA 3): ");
 
         // Formar a frase da tabela 3
         for ($i=0; $i < $data['numTableRL']; $i++) { 
             if (isset($data['checkRL'.$i])) {
 
-                $phrase = $this->formPhraseAndQueryTableType($data, $data['checkRL'.$i], 'RL', $i, $phrase, $query);
+                $phrase = $this->formPhraseTableType($data, $data['checkRL'.$i], 'RL', $i, $phrase, $query);
             }
         }
 
         // Formar a frase da tabela 4
         for ($i=0; $i < $data['numTableER']; $i++) { 
             if (isset($data['checkER'.$i])) {                
-                $phrase = $this->formPhraseAndQueryTableType($data, $data['checkER'.$i], 'ER', $i, $phrase, $query);
+                $phrase = $this->formPhraseTableType($data, $data['checkER'.$i], 'ER', $i, $phrase, $query);
             }
         }
 
         return $phrase;
     }
 
-    public function formPhraseAndQueryTableType($data, $idProperty, $type, $position, $phrase, &$query1) {
-        $language_id = '1';
-
-        $property = Property::with(['language' => function($query) use ($language_id) {
-                                $query->where('language_id', $language_id);
-                            }])
-                            ->with(['entType' => function($query) use ($language_id) {
-                                $query->with(['language' => function($query) use ($language_id) {
-                                    $query->where('language_id', $language_id);
-                                }]);
-                            }])
-                            ->with(['relType' => function($query) use ($language_id) {
-                                $query->with(['language' => function($query) use ($language_id) {
-                                    $query->where('language_id', $language_id);
-                                }]);
-                            }])
-                            ->find($idProperty);
+    public function formPhraseTableType($data, $idProperty, $type, $position, $phrase, &$query1) {
+        $property = $this->getPropertyData($idProperty);
 
         $nameProp  = $property->language->first()->pivot->name;
         $valueType = $property->value_type;
@@ -396,13 +431,74 @@ class DynamicSearchController extends Controller
         //if ($valueQuery != "") {
 
             // Adicionar a query filtros de pesquisa de acordo com as opções selecionadas
-            $query1 = $query1->whereHas('values', function($q) use ($operatorQuery, $valueQuery, $idProperty) {
-                $q->where('value', $operatorQuery, $valueQuery)->where('property_id', $idProperty);
-            }); 
+            /*$query1 = $query1->whereHas('values', function($q) use ($operatorQuery, $valueQuery, $idProperty) {
+                                $q->where('property_id', $idProperty)->where('value', $operatorQuery, $valueQuery);
+                            })->with(['values.property' => function($query) use ($valueQuery, $idProperty) {
+                                $query->where('id', $idProperty);
+                            }])->with(['values' => function($query) use ($valueQuery, $idProperty) {
+                                $query->where('property_id', $idProperty);
+                            }]);*/
 
         //}
 
         return $phrase;
+    }
+
+    public function formQueryTable1AndTable2($data, $idProperty, $type, $position, &$queryTable1) {
+        $language_id = '1';
+
+        $property  = $this->getPropertyData($idProperty);
+        $valueType = $property->value_type;
+
+        $valueQuery    = '';
+        $operatorQuery = '=';
+        if ($valueType == "int") {
+            $valueQuery    = $data['int'.$type.$position];
+            $operatorQuery = $data['operators'.$type.$position];
+        }  else if ($valueType == "double") {
+            $valueQuery    = $data['double'.$type.$position];
+            $operatorQuery = $data['operators'.$type.$position];
+        } else  if ($valueType == "text") {
+            $valueQuery = $data['text'.$type.$position];
+        } else  if ($valueType == "enum") {
+            $valueQuery = $data['select'.$type.$position];
+        } else  if ($valueType == "bool") {
+            $valueQuery = $data['radio'.$type.$position];
+        }
+
+        $queryTable1 = $queryTable1->whereHas('values', function($q) use ($idProperty, $operatorQuery, $valueQuery) {
+                                        $q->where('property_id', $idProperty)->where('value', $operatorQuery, $valueQuery);
+                                    });
+    }
+
+    public function formatArrayData($data, $keySelect) {
+        $array = [];
+        foreach ($data as $value) {
+            $array[] = $value[$keySelect];
+        }
+
+        return $array;
+    }
+
+    public function getPropertyData($idProperty) {
+        $language_id = '1';
+
+        $property = Property::with(['language' => function($query) use ($language_id) {
+                                $query->where('language_id', $language_id);
+                            }])
+                            ->with(['entType' => function($query) use ($language_id) {
+                                $query->with(['language' => function($query) use ($language_id) {
+                                    $query->where('language_id', $language_id);
+                                }]);
+                            }])
+                            ->with(['relType' => function($query) use ($language_id) {
+                                $query->with(['language' => function($query) use ($language_id) {
+                                    $query->where('language_id', $language_id);
+                                }]);
+                            }])
+                            ->find($idProperty);
+
+        return $property;
     }
 
     public function getNameEntType($idEntType) {
